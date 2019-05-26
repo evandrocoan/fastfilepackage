20bedaaa825f861f48b7dc4d45694c38fbe3f0c0
evandrocoan <evandrocoan@hotmail.com>
2019-05-25 21:03:13 -0300

    Fixed unfair benchmark when FastFile is just yielding an empty
    string.

--- tests/fastfileperformance.py
+++ tests/fastfileperformance.py
@@ -8,8 +8,8 @@ testfile = './myfile.log'
 timenow = time.time()
 with open( testfile, 'r', errors='replace' ) as myfile:
     for item in myfile:
-        if 'word' in item:
-            pass
+        if None:
+            var = item
 
 python_time = time.time() - timenow
 timedifference = datetime.timedelta( seconds=python_time )
@@ -18,8 +18,8 @@ print( 'Python   timedifference', timedifference, flush=True )
 timenow = time.time()
 iterable = fastfilepackage.FastFile( testfile )
 for item in iterable:
-    if 'word' in item:
-        pass
+    if None:
+        item = var
 
 fastfile_time = time.time() - timenow
 timedifference = datetime.timedelta( seconds=fastfile_time )
